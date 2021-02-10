1696c85fee2fc6077e138dd73920c3b81ae231a8
lcdf <9001053+fedriz@users.noreply.github.com>
2021-02-10 11:29:32 +0100

    Cleaning

--- code/api/hereMaps.js
+++ code/api/hereMaps.js
@@ -1,25 +1,3 @@
-// export async function requestGetCoordinatesFromSearch(query) {
-//   const r = 150 * 1000;
-//   try {
-//     if (query) {
-//       const response = await fetch(
-//         `https://places.ls.hereapi.com/places/v1/browse?apiKey=${process.env.TILES_API_KEY}&in=46.31,11.26;r=${r}&q=${query}`,
-//         {
-//           method: "GET",
-//           headers: new Headers({
-//             Accept: "application/json",
-//           }),
-//         }
-//       );
-//       const data = await response.json();
-//       this.searchPlacesFound = data.results.items;
-//     }
-//   } catch (error) {
-//     this.searchPlacesFound = [];
-//   }
-// }
-
-// export async function request__get_coordinates_from_search(query) {
 export async function requestGetCoordinatesFromSearch(query) {
   const r = 150 * 1000;
   try {
