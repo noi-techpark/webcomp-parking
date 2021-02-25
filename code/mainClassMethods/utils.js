b4ee11708c9fb52b35f5b05f659b9d1e6dccf076
lcdf <9001053+fedriz@users.noreply.github.com>
2021-02-25 15:58:54 +0100

    Handle greyIcon in getPin

--- code/mainClassMethods/utils.js
+++ code/mainClassMethods/utils.js
@@ -1,13 +1,16 @@
 import greenIcon from "../assets/pins/green.svg";
 import redIcon from "../assets/pins/red.svg";
-// import greyIcon from "../assets/pins/grey.svg";
+import greyIcon from "../assets/pins/grey.svg";
 // import orangeIcon from "../assets/pins/orange.svg";
 
 export const getPin = (mvalue) => {
-  if (mvalue >= 1) {
-    return greenIcon;
+  if (mvalue !== undefined) {
+    if (mvalue >= 1) {
+      return greenIcon;
+    }
+    if (mvalue <= 0) {
+      return redIcon;
+    }
   }
-  if (mvalue <= 0) {
-    return redIcon;
-  }
+  return greyIcon;
 };
